95660ff3fbb087a709801fb9c3e96cd7c95a2a48
Ruth <ruth.mcilwaine@myob.com>
2022-06-02 17:12:32 +1000

    refactor

--- day1part1.js
+++ day1part1.js
@@ -1,12 +1,12 @@
-var { report } = require(`./report.js`);
-//let report = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+//var { report } = require(`./report.js`);  `890P[]
+let report = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 let counter = 0;
 
 for (let i = 0; i < report.length; i++) {
   let currNum = report[i];
   let prevNum = report[i - 1];
 
-  if (currNum === report[0]) {
+  if (i === 0) {
     console.log(`${currNum} N/A - no previous measurement`);
   } else if (currNum > prevNum) {
     counter++;
