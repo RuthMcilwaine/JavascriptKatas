181f6591566363b092f547a493fea7ecec25668a
Ruth <ruth.mcilwaine@myob.com>
2022-07-20 14:41:01 +1000

    working with small input string

--- day2/day2.js
+++ day2/day2.js
@@ -1,48 +1,54 @@
-(() => {
-  let distance = 0;
-  let depth = 0;
-  let arr = [];
-  let obj = {};
+// (() => { anon arrow function
+// })();
+//var { input } = require(`./input.js`);
+
+const convertInput = (input) => {
+  let command = input.split('\n ');
+  arr = [];
+  command.forEach((element) => {
+    let output = element.split(' ');
+    let obj = {};
+
+    obj.direction = output[0];
+    obj.distance = parseInt(output[1]);
+    arr.push(obj);
+  });
+  return arr;
+};
+
+// use global variables without breaking encapsulation
+function main() {
   let input = `forward 5
  down 5
  forward 8
  up 3
  down 8
  forward 2`;
+  let converted = convertInput(input);
+  console.log(calculateValue(converted));
+}
+//declare inside function or pass in as arguments
+function calculateValue(arr) {
+  let distance = 0;
+  let depth = 0;
+  arr.forEach((command) => {
+    let value = command.distance;
 
-  const convertInput = (input) => {
-    let command = input.split('\n ');
-    //console.log(command);
-    command.forEach((element) => {
-      let output = element.split(' ');
-      //console.log(output);
-
-      obj.direction = output[0];
-      obj.distance = parseInt(output[1]);
-      //console.log(obj);
-      arr.push(obj);
-      console.log(`arr` + arr.toString());
-    });
-
-    function calculateValue(arr) {
-      arr.forEach((element) => {
-        switch (command) {
-          case 'up':
-            depth -= value;
-            break;
-          case 'down':
-            depth += value;
-            break;
-          case 'forward':
-            distance += value;
-            break;
-        }
-      });
+    switch (command.direction) {
+      case 'up':
+        depth -= value;
+        break;
+      case 'down':
+        depth += value;
+        break;
+      case 'forward':
+        distance += value;
+        break;
     }
-    console.log(`depth * distance` + depth * distance);
+  });
+  console.log(distance);
+  console.log(depth);
+  return distance * depth;
+}
 
-    console.log(`calculateValue(arr)` + calculateValue(arr));
-  };
-
-  console.log(`convertInput(input)` + convertInput(input));
-})();
+main();
